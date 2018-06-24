9281f01d149525caf784c1153ef4db40f7242402
Edmund Wu <fangkazuto@gmail.com>
2018-06-23 22:01:51 -0400

    Use custom dwm

--- snowroot/etc/overlay/default.nix
+++ snowroot/etc/overlay/default.nix
@@ -4,6 +4,5 @@
   (import /etc/nixos/overlay/pkgs/glava.nix)
   (import /etc/nixos/overlay/pkgs/ncmpcpp.nix)
   (import /etc/nixos/overlay/pkgs/polybar.nix)
-  (import /etc/nixos/overlay/pkgs/st.nix)
   (import /etc/nixos/overlay/pkgs/vscode-with-extensions.nix)
 ]
--- snowroot/etc/overlay/pkgs/dwm.nix
+++ snowroot/etc/overlay/pkgs/dwm.nix
@@ -1,35 +1,14 @@
 self: super:
 
 let
-  inherit (super) fetchurl;
+  inherit (super) fetchgit;
   settings = import /etc/nixos/settings.nix;
 in with settings; {
-  dwm = super.dwm.override {
-    patches = [
-      # config.h
-      (/. + "${HOME}/.dwm/config.h.diff")
-      # config.mk
-      (/. + "${HOME}/.dwm/config.mk.diff")
-      # cyclelayouts
-      (fetchurl {
-        url = https://dwm.suckless.org/patches/cyclelayouts/dwm-cyclelayouts-20180524-a09e766.diff;
-        sha256 = "1ijslwbfmmznv4m5hadra8jcrds4zwky2m98d7cg8zdz3s9rva4q";
-      })
-      # fibonacci
-      (fetchurl {
-        url = https://dwm.suckless.org/patches/fibonacci/dwm-fibonacci-5.8.2.diff;
-        sha256 = "06qavwzi9hgg3vcwfsrc520gwd4dcmawcsamcryhbisdbss2kr37";
-      })
-      # pertag
-      (fetchurl {
-        url = https://dwm.suckless.org/patches/pertag/dwm-pertag-6.1.diff;
-        sha256 = "0sdlnwz1048hx4qw5mp1rf10npah9sg87w2g28a143pvw1srnzgv";
-      })
-      # ewmhtags
-      (fetchurl {
-        url = https://dwm.suckless.org/patches/ewmhtags/dwm-ewmhtags-6.1.diff;
-        sha256 = "1gzf48bxi5hxv918j6isyj21z1xkgrch2zzaslwnf1hbvydnsz34";
-      })
-    ];
-  };
+  dwm = super.dwm.overrideAttrs (oldAttrs {
+    src = fetchgit {
+      url = https://gitlab.com/eadwu/dwm;
+      rev = "63affd3ee071f3e08d374164e67de39922e8b805";
+      sha256 = "094cssy6ccbj3p0w4ggjvq12rpfh859ip48kfnxhk1c7nvyyc9cr";
+    };
+  });
 }
