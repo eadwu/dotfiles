94cf19e31a298cd05d91d04ca8931435515a228b
Edmund Wu <fangkazuto@gmail.com>
2018-06-23 22:06:19 -0400

    Allow for overwritten src

--- snowroot/etc/overlay/pkgs/dwm.nix
+++ snowroot/etc/overlay/pkgs/dwm.nix
@@ -4,7 +4,7 @@ let
   inherit (super) fetchgit;
   settings = import /etc/nixos/settings.nix;
 in with settings; {
-  dwm = super.dwm.overrideAttrs (oldAttrs {
+  dwm = super.dwm.overrideAttrs (oldAttrs: rec {
     src = fetchgit {
       url = https://gitlab.com/eadwu/dwm;
       rev = "63affd3ee071f3e08d374164e67de39922e8b805";
