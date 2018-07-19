d257d4ec74eadd58917560a476e2e3333fac5369
Edmund Wu <fangkazuto@gmail.com>
2018-07-19 15:21:35 -0400

    Prevent polybar -s from drawing a window (https://github.com/jaagr/polybar/issues/1314)

--- snowroot/etc/overlay/pkgs/polybar.nix
+++ snowroot/etc/overlay/pkgs/polybar.nix
@@ -1,7 +1,7 @@
 self: super:
 
 let
-  inherit (super) fetchgit;
+  inherit (super) fetchgit fetchpatch;
 in {
   polybar = (super.polybar.override {
     githubSupport = true;
@@ -12,5 +12,12 @@ in {
       rev = "028b1413ef9490cdcb21348ea0ca704828ef538e";
       sha256 = "01mqwnlq7d9c503c5cnc19hl7hvk03fmih1wis7zljxdiz5hb82d";
     };
+
+    patches = [
+      (fetchpatch {
+        url = https://github.com/jaagr/polybar/pull/1322.patch;
+        sha256 = "1xvk5qhvg52x1fyqk7dw3f4w4hi595s3sa3qdg493vn2x3zzxfkq";
+      })
+    ];
   });
 }
