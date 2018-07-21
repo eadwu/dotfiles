17a54c4c08b57cacbf69025c86fb24538604a276
Edmund Wu <fangkazuto@gmail.com>
2018-07-21 14:50:34 -0400

    Introduce general power saving configuration

--- snowroot/etc/boot.nix
+++ snowroot/etc/boot.nix
@@ -7,12 +7,32 @@ in with settings; {
     cleanTmpDir = true;
     kernelPackages = pkgs.linuxPackages_testing;
 
+    blacklistedKernelModules = [
+      # https://wiki.archlinux.org/index.php/Improving_performance#Watchdogs
+      "iTCO_wdt"
+    ];
+
     kernel = {
       sysctl = {
         "fs.inotify.max_user_instances" = 1024;
         "fs.inotify.max_user_watches" = 524288;
       };
     };
+
+    kernelParams = [
+      # https://wiki.archlinux.org/index.php/Improving_performance#Watchdogs
+      "nowatchdog"
+
+      # https://ivanvojtko.blogspot.com/2016/04/how-to-get-longer-battery-life-on-linux.html
+      "i915.enable_fbc=1"
+
+      # https://wiki.archlinux.org/index.php/Power_management
+      # Debugging feature to catch hardware hangs that cause a kernel panic
+      # Disabling may cause decrease in power usage
+      "nmi_watchdog=0"
+      "snd_hda_intel.power_save=1"
+      "vm.dirty_writeback_centisecs=6000"
+    ];
 
     loader = {
       efi = {
