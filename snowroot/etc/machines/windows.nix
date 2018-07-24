cf56521e370654d8a7f7e4fc8384d8763e442463
Edmund Wu <fangkazuto@gmail.com>
2018-07-24 14:57:44 -0400

    Adjust power-saving options

--- snowroot/etc/machines/windows.nix
+++ snowroot/etc/machines/windows.nix
@@ -8,11 +8,18 @@
     ];
 
   boot = {
+    blacklistedKernelModules = [
+      # Disable integrated webcam
+      "uvcvideo"
+    ];
+
     kernelParams = [
-      "acpi_rev_override=1"
-      # i915 kernel power saving
-      "enable_psr=1"
-      "disable_power_well=0"
+      "acpi_rev_override=5"
+      "i915.enable_dc=1"
+      "i915.enable_guc=-1"
+      "i915.enable_psr=1"
+      "i915.disable_power_well=0"
+      "psmouse.synaptics_intertouch=0"
     ];
   };
 
@@ -31,6 +38,15 @@
 
   i18n = {
     consoleFont = "latarcyrheb-sun32";
+  };
+
+  powerManagement = {
+    undervolt = {
+      enable = true;
+      core = -100;
+      cache = -100;
+      gpu = -50;
+    };
   };
 
   services = {
