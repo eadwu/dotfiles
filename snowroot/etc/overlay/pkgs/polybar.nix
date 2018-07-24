01f33276d29c055f5fabb8aae7deda2b8d1143b6
Edmund Wu <fangkazuto@gmail.com>
2018-07-24 14:50:44 -0400

    Remove patch since polybar/master merged

--- snowroot/etc/overlay/pkgs/polybar.nix
+++ snowroot/etc/overlay/pkgs/polybar.nix
@@ -10,12 +10,8 @@ in {
   }).overrideAttrs (oldAttrs: rec {
     src = fetchgit {
       url = https://github.com/jaagr/polybar;
-      rev = "01fc545e096052dee2984fbbc985727afe38fca3";
-      sha256 = "0v471id84nvp1l542ymjmnljc7apkcgx7cr017prra22cppsiimp";
+      rev = "4cb0b1838939d78295e8f9acd32e9cf58865a798";
+      sha256 = "18msm8ffpy2kdms2lxmj92f338vrlls23j5s7m9gspf10ivv2w1w";
     };
-
-    patches = [
-      /etc/nixos/overlay/patches/1322.patch
-    ];
   });
 }
