ce180bff89e06f4d564a7db6c9715b17cbd88bee
Edmund Wu <fangkazuto@gmail.com>
2018-07-28 17:51:59 -0400

    Improve polybar overlay
    - Remove useless keyword `rec`
    - Inherit `fetchgit` and `fetchgit` from `self.pkgs`

--- snowroot/etc/overlay/pkgs/polybar.nix
+++ snowroot/etc/overlay/pkgs/polybar.nix
@@ -1,13 +1,13 @@
 self: super:
 
 let
-  inherit (super) fetchgit fetchpatch;
+  inherit (self.pkgs) fetchgit fetchpatch;
 in {
   polybar = (super.polybar.override {
     githubSupport = true;
     mpdSupport = true;
     pulseSupport = true;
-  }).overrideAttrs (oldAttrs: rec {
+  }).overrideAttrs (oldAttrs: {
     src = fetchgit {
       url = https://github.com/jaagr/polybar;
       rev = "4cb0b1838939d78295e8f9acd32e9cf58865a798";
