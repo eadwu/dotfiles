669e37c894043e2aa74a2167f07cf20e4d229d57
Edmund Wu <fangkazuto@gmail.com>
2018-08-11 22:29:20 -0400

    Set emacs-nox alias for terminal usage

--- snowroot/etc/programs.nix
+++ snowroot/etc/programs.nix
@@ -50,6 +50,7 @@ in with settings; {
       '';
       shellAliases = {
         "download-audio" = "youtube-dl --extract-audio --audio-format mp3";
+        "emacs-nox" = "emacs --no-window-system";
         "nixos-rebuild-local" = "nixos-rebuild -I nixpkgs=${HOME}/Downloads/nixpkgs";
         "nixos-generate-iso" = ''nix-build "<nixpkgs/nixos>" -A config.system.build.isoImage -I nixos-config="${HOME}/Downloads/dotfiles/snowroot/etc/iso.nix" -I nixpkgs="${HOME}/Downloads/nixpkgs"'';
         "passhash" = ''openssl passwd -1 -salt "$(od -vAn -N4 -tu4 < /dev/urandom)"'';
