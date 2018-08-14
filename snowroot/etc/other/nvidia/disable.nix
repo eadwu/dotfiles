1c69ad56d2132b9060fe1b90cd217c5e47a620af
Edmund Wu <fangkazuto@gmail.com>
2018-08-14 09:52:41 -0400

    Prefer a non-bbswitch approach for disabling Nvidia

--- snowroot/etc/other/nvidia/disable.nix
+++ snowroot/etc/other/nvidia/disable.nix
@@ -1,13 +1,26 @@
 { config, pkgs, ... }:
 
 {
-  hardware = {
-    nvidiaOptimus = {
-      disable = true;
-    };
+  boot = {
+    blacklistedKernelModules = [
+      "nouveau"
+      "nvidiafb"
+
+      "nvidia"
+      "nvidia-uvm"
+      "nvidia_drm"
+      "nvidia_modeset"
+    ];
   };
 
   services = {
+    udev = {
+      extraRules = ''
+        # Runtime PM for PCI Device NVIDIA Corporation GP107M [GeForce GTX 1050 Ti Mobile]
+        ACTION=="add", SUBSYSTEMS=="pci", ATTRS{device}=="0x1901", ATTRS{vendor}=="0x8086", TEST=="power/control", ATTR{power/control}:="auto"
+      '';
+    };
+
     xserver = {
       videoDrivers = [
         "modesetting"
