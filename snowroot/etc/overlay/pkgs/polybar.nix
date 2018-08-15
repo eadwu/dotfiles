c1d190c15ccbb9d2dd157b32ce7f5dce5eb77148
Edmund Wu <fangkazuto@gmail.com>
2018-08-15 11:54:18 -0400

    polybar: b632e7d -> 2a3b2b2

--- snowroot/etc/overlay/pkgs/polybar.nix
+++ snowroot/etc/overlay/pkgs/polybar.nix
@@ -10,8 +10,8 @@ in {
   }).overrideAttrs (oldAttrs: {
     src = fetchgit {
       url = https://github.com/jaagr/polybar;
-      rev = "b632e7da38cc07c4b7fc6613e6f64d99533e3a81";
-      sha256 = "09bdmmb3mwsb3ijqmqdf1ix4yqjmx26dq4p6dzzvxxsq6gndq7lq";
+      rev = "2a3b2b2b998d84df56800cd196315307d9d481fb";
+      sha256 = "0yhmh50l0cmi3ib25jpf9j9gl24d83jzvfn0h3wibiqfdm4g6qh6";
     };
   });
 }
