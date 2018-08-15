4f9b035720cae0ee7f1c7c30beb8a7d5cab08540
Edmund Wu <fangkazuto@gmail.com>
2018-08-15 11:53:40 -0400

    Cleanup
    - Remove useless section
    - Remove unnecessary import and keywords

--- snowroot/etc/machines/windows.nix
+++ snowroot/etc/machines/windows.nix
@@ -49,13 +49,6 @@ in {
       '';
     };
 
-    udev = {
-      extraRules = ''
-        # Runtime PM for PCI Device NVIDIA Corporation GP107M [GeForce GTX 1050 Ti Mobile]
-        # ACTION=="add", SUBSYSTEMS=="pci", ATTRS{device}=="0x1901", ATTRS{vendor}=="0x8086", TEST=="power/control", ATTR{power/control}:="auto"
-      '';
-    };
-
     undervolt = {
       enable = true;
       coreOffset = "-100";
--- snowroot/etc/overlay/pkgs/suckless.nix
+++ snowroot/etc/overlay/pkgs/suckless.nix
@@ -2,23 +2,22 @@ self: super:
 
 let
   inherit (super) fetchgit;
-  settings = import /etc/nixos/settings.nix;
-in with settings; {
-  dmenu = super.dmenu.overrideAttrs (oldAttrs: rec {
+in {
+  dmenu = super.dmenu.overrideAttrs (oldAttrs: {
     src = fetchgit {
       url = https://gitlab.com/eadwu/dmenu;
       rev = "eae2a506dab7168d030fb43a2fcb6c79fd601d8f";
       sha256 = "0a143vw1nap80xzbasqy0r82ky4v6ah1ffn4mfmjxwg3m82j00np";
     };
   });
-  dwm = super.dwm.overrideAttrs (oldAttrs: rec {
+  dwm = super.dwm.overrideAttrs (oldAttrs: {
     src = fetchgit {
       url = https://gitlab.com/eadwu/dwm;
       rev = "ecfb90a00f3f0be582984942a94ad5fd2f3c0502";
       sha256 = "1dh8ba16s0a1nfqla18b69nkp32a9gs11gvfhhgmns7qr3gliqyd";
     };
   });
-  st = super.st.overrideAttrs (oldAttrs: rec {
+  st = super.st.overrideAttrs (oldAttrs: {
     src = fetchgit {
       url = https://gitlab.com/eadwu/st;
       rev = "f43aa9382c0cc7a46a6a8d6e4b0c030ad8dce8e1";
