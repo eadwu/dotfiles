2eb7e31d7a8c766eca3e37be3796471cf58a1c86
Edmund Wu <fangkazuto@gmail.com>
2018-09-04 15:29:12 -0400

    emacs: ca8dbde -> 21637d5

--- snowroot/etc/overlay/pkgs/emacs.nix
+++ snowroot/etc/overlay/pkgs/emacs.nix
@@ -11,8 +11,8 @@ let
   }).overrideAttrs (oldAttrs: {
     src = fetchgit {
       url = https://git.savannah.gnu.org/git/emacs.git;
-      rev = "ca8dbde14b2b2025aa69c52378904ee5c573a0d7";
-      sha256 = "0vsy6i9ca5g3y7fg171y3vpiam5cwdc6bb9jhxfajw2714g6r5ir";
+      rev = "21637d5e5b29d5ec8fb966c0ddfbfba3eb33da38";
+      sha256 = "1cgilqgw78bq0gxqfivnpvfwlg1b5h38fcsnw78hd4266gnxn3nl";
     };
 
     buildInputs = oldAttrs.buildInputs ++ (with pkgs; [
