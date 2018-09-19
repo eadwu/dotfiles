18e6635f77b3f33c90bff6f9c6c0226ecf2db7f0
Edmund Wu <fangkazuto@gmail.com>
2018-09-19 14:24:05 -0400

    Move nix configuration for Emacs

--- snowblocks/nixpkgs/emacs/modules/lang/nix.el
+++ snowblocks/nixpkgs/emacs/modules/lang/nix.el
@@ -1,6 +1,5 @@
 (use-package nix-mode
+  :config
+  (add-to-list 'auto-mode-alist '("\\.nix\\'" . nix-mode))
   :custom
   (nix-indent-function 'nix-indent-line))
-
-(use-package nix-update
-  :bind (("C-. u" . nix-update-fetch)))
