88c7b69c1874d5e0137172f5d7d35faa60d2c8ca
Edmund Wu <fangkazuto@gmail.com>
2018-09-22 10:13:03 -0400

    Downgrade to gtk2 xfce applications

--- snowblocks/nixpkgs/common.nix
+++ snowblocks/nixpkgs/common.nix
@@ -21,10 +21,10 @@
     packages = with pkgs; [
       # Environment
       nitrogen
-      xfceUnstable.thunar
-      xfceUnstable.xfce4-notifyd
-      xfceUnstable.xfce4-screenshooter
-      xfceUnstable.xfce4-taskmanager
+      xfce.thunar
+      xfce.xfce4-notifyd
+      xfce.xfce4-screenshooter
+      xfce.xfce4-taskmanager
       ## Theme
       gtk-engine-murrine
 
