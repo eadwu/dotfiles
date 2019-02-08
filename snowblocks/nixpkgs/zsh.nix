645d93bb01e814721e8e6fd50dd72ca4e93d5f8f
Edmund Wu <fangkazuto@gmail.com>
2019-02-07 21:59:49 -0500

    zsh: include function to upload system to cachix

--- snowblocks/nixpkgs/zsh.nix
+++ snowblocks/nixpkgs/zsh.nix
@@ -23,6 +23,21 @@
           docker tag "$1" "''${DOCKER_ID_USER}/$1"
           docker push "''${DOCKER_ID_USER}/$1"
         }
+
+        nix-upload-system () {
+          {
+            nix-build "<nixpkgs/nixos>" \
+              -A config.system.build.toplevel \
+              --no-out-link;
+
+            nix-build "<home-manager/home-manager/home-manager.nix>" \
+              -I home-manager=https://github.com/rycee/home-manager/archive/master.tar.gz \
+              --argstr confAttr "" \
+              --argstr confPath "$HOME/.config/nixpkgs/home.nix" \
+              -A activationPackage \
+              --no-out-link;
+          } | cachix push system
+        }
       '';
     };
   };
