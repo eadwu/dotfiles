26cc6ab8894ac2ff83f51589a3a8a8048664ef7e
Edmund Wu <fangkazuto@gmail.com>
2019-03-03 20:27:00 -0500

    gitignore: 2019-03-03

--- snowblocks/nixpkgs/git.nix
+++ snowblocks/nixpkgs/git.nix
@@ -4,14 +4,14 @@ let
   gitignore = pkgs.stdenv.mkDerivation {
     name = "gitignore";
 
-    outputHash = "3401wmbz1xa7pn5g3ym71s17jpd2xix57bmg1lh2sv7pdpfv074z3rfw1zl7sj7ipx4mdvrsz231i2g19q42akraj1n51rr7mhnc332";
+    outputHash = "044iccjc4vz4rz3hc2mqls48ir8llx0pxhv0jk1algarihgxhlhwj2mraswnfc0ckg0ss53ckcrlkq4cxsm1w5ql200jpqqb8jdl2ww";
     outputHashAlgo = "sha512";
     outputHashMode = "flat";
 
     buildInputs = lib.singleton pkgs.wget;
 
     buildCommand = ''
-      wget "https://www.gitignore.io/api/c,r,git,c++,java,macos,linux,latex,elisp,emacs,cmake,intellij,database,visualstudiocode" \
+      wget "https://www.gitignore.io/api/c,r,git,c++,java,rust,cmake,elisp,emacs,latex,linux,macos,haskell,database,intellij,visualstudiocode" \
         --ca-certificate=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt \
         -O $out
     '';
